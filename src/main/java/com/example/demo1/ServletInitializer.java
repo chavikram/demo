e4b96ba522584f6acf117ebb33d06f7d8978d2be
vikram <chavikram@gmail.com>
2022-10-18 15:17:41 +0530

    second commit

--- src/main/java/com/example/demo1/ServletInitializer.java
+++ src/main/java/com/example/demo1/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo1;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
